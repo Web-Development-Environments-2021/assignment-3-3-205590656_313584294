babc5916f4efb19abc7807af6d914312679c50b7
Barmocha <barmocha@post.bgu.ac.il>
2021-07-07 22:25:03 +0300

    league_detalis

--- src/components/LeagueInfo.vue
+++ src/components/LeagueInfo.vue
@@ -6,11 +6,35 @@
       style="max-width: 20rem;"
       class="mb-2"
     >
-      <b-card-title>{{leagueName}}</b-card-title>
+      <b-card-title> {{ leagueName }}</b-card-title>
       <b-card-text>
         Season: {{ season }}
         <br/>
         Stage: {{ stage }}
+        <br/>
+        <br/>        
+        Next Game:
+        <br/>
+        Home team id: {{ next[0].home_team }}
+        <br/>        
+        Away team id: {{ next[0].away_team }}
+        <br/>
+        Date: {{ next[0].date }}
+        <br/>
+        Time: {{ next[0].time }}
+        <br/>
+        Stage: {{ next[0].stage }}
+        
+      <!-- <GamePreview
+        v-for="g in next"
+        :homeTeam="g.home_team" 
+        :awayTeam="g.away_team" 
+        :date="g.date" 
+        :time="g.time"
+        :stage="g.stage" 
+        :key="g.id"></GamePreview> -->
+
+
       </b-card-text>
       <b-button href="#" variant="primary">Go somewhere</b-button>
     </b-card>
@@ -19,12 +43,38 @@
 
 <script>
 export default {
- data() {
+  data() {
     return {
-      leagueName: "superliga", 
-      season: "season", 
-      stage: "stage"
+      leagueName:'', 
+      season:'', 
+      stage:'',
+      next:'',
     };
+  },
+  methods: {
+    async leagueDetails() {
+      console.log("response");
+      try {
+        const response = await this.axios.get(
+        this.$root.store.BASE_URL + "/league/getDetails",
+        );
+          
+        let info = response.data;
+        this.leagueName = info.league_name;
+        this.season = info.current_season_name;
+        this.stage = info.current_stage_name;
+        this.next = info.next_game;        
+        
+        console.log(response);
+      } catch (error) {
+          console.log("error in update league");
+          console.log(error);
+        }
+      }
+  },
+  mounted(){
+    console.log("league details mounted");
+    this.leagueDetails();
   },
 }
 </script>
@@ -32,8 +82,8 @@ export default {
 <style>
 .league-preview {
   display: inline-block;
-  width: 250px;
-  height: 200px;
+  width: 300px;
+  height: 385px;
   position: relative;
   margin: 10px 10px;
   border-style: solid;
