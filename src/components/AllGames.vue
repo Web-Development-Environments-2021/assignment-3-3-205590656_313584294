e0ac10cc7b0f91f64f5076fef05292d7ea471a97
Barmocha <barmocha@post.bgu.ac.il>
2021-07-08 21:49:11 +0300

    allGame

--- src/components/AllGames.vue
+++ src/components/AllGames.vue
@@ -1,13 +1,32 @@
 <template>
   <div>
-    <GamePreview
-      v-for="g in games"
-      :id="g.game_id" 
-      :hostTeam="g.home_team" 
-      :guestTeam="g.away_team" 
-      :date="g.date" 
-      :time="g.time" 
-      :key="g.id"></GamePreview>
+    <b-row>
+        <b-col>
+            <h2>Past Games</h2>
+            <GamePreview
+            v-for="g in p_games"
+            :id="g.game_id" 
+            :hostTeam="g.home_team" 
+            :guestTeam="g.away_team" 
+            :date="g.date" 
+            :time="g.time" 
+            :key="g.id">
+            </GamePreview>  
+        </b-col>
+        <b-col>
+            <h2>Future Games</h2>
+            <GamePreview
+            v-for="g in f_games"
+            :id="g.game_id" 
+            :hostTeam="g.home_team" 
+            :guestTeam="g.away_team" 
+            :date="g.date" 
+            :time="g.time" 
+            :key="g.id">
+            </GamePreview>  
+        </b-col>
+    </b-row>
+    
   </div>
 </template>
 
@@ -20,7 +39,8 @@ export default {
   }, 
   data() {
     return {
-      games: [],
+      p_games: [],
+      f_games:[],
     };
   },
   methods: {
@@ -31,18 +51,34 @@ export default {
             this.$root.store.BASE_URL + "/game/pastGames",
         );
         const games = response.data;
-        this.games = [];
-        this.games.push(...games);
+        this.p_games = [];
+        this.p_games.push(...games);
         console.log(response);
       } catch (error) {
         console.log("error in update games")
         console.log(error);
       }
-    }
+    },
+    async futuerGames(){
+      console.log("response");
+      try {
+        const response = await this.axios.get(
+            this.$root.store.BASE_URL + "/game/currentCycleGames",
+        );
+        const games = response.data;
+        this.f_games = [];
+        this.f_games.push(...games);
+        console.log(response);
+      } catch (error) {
+        console.log("error in update games")
+        console.log(error);
+      }
   }, 
+  },
   mounted(){
     console.log("favorite games mounted");
     this.pastGames(); 
+    this.futuerGames();
   }
 };
 </script> 
