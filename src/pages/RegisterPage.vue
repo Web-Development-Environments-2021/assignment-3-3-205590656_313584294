8f7c71081d519b66d5340db03e405356218ac563
Nicole Dezoraive <nicoled@post.bgu.ac.il>
2021-07-10 01:34:35 +0300

    .

--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -295,7 +295,6 @@ export default {
     async Register() {
       try {
         const response = await this.axios.post(
-          // "https://test-for-3-2.herokuapp.com/user/Register",
           this.$root.store.BASE_URL + "/Register",
           {
             username: this.form.username,
